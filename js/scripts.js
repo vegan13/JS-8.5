28ee1b4ce7174a1e9c0016999698c995516ceea3
vegan13 <vegan13@o2.pl>
2017-02-26 22:23:53 +0100

    instr. warunkowa

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var a = 1,
+    h = 1,
+    triangleArea;
+
+a = prompt('Wpisz długość podstawy trójkąta');
+h = prompt('Wpisz wysokość trójkąta');
+
+triangleArea = a * h / 2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
